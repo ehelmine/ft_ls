9a53fcd5ffc22374fb5b2d5249f213a178e8d42b
ehelmine <ella.helminen@outlook.com>
2021-07-14 15:55:21 +0300

    exit fix

--- exit_function.c
+++ exit_function.c
@@ -6,7 +6,7 @@
 /*   By: ehelmine <ehelmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 14:15:24 by ehelmine          #+#    #+#             */
-/*   Updated: 2021/07/14 14:13:08 by ehelmine         ###   ########.fr       */
+/*   Updated: 2021/07/14 15:54:42 by ehelmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	ft_exit_call(int i, char c)
 {
 	if (i == 1)
 	{
-		write(1, "ft_ls: illegal option -- ", 23);
+		write(1, "ft_ls: illegal option -- ", 26);
 		write(1, &c, 1);
-		write(1, "\nusage: ft_ls [-Ralrt] [file ...]\n", 31);
+		write(1, "\nusage: ft_ls [-Ralrt] [file ...]\n", 34);
 	}
 	exit (1);
 }
